a7fee0bdf0d6d59b4689700eb1ea4eb2102adff5
aurakhov <aurakhovtel@gmail.com>
2023-11-29 14:29:23 +0500

    LR7-thymeleaf

---
+++ src/main/java/ru/aurakhov/lr7thymeleafwebapp/Lr7ThymeleafWebappApplication.java
@@ -0,0 +1,13 @@
+package ru.aurakhov.lr7thymeleafwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lr7ThymeleafWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lr7ThymeleafWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/aurakhov/lr7thymeleafwebapp/Lr7ThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aurakhov.lr7thymeleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lr7ThymeleafWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
